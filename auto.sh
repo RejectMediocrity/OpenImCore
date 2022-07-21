4dec6bdd5e7006d6647dc7255fa9f67445486bb3
Logan <liubin3310@gmail.com>
2022-07-21 18:40:24 +0800

    修改脚本文件

--- auto.sh
+++ auto.sh
@@ -1,32 +1,28 @@
 #! /bin/sh
 
-git_commit_des="升级sdk"
+git_commit_des="升级SDK"
 
-echo "\n ****** begin ****** \n"
+echo "\n ---- 开始升级SDK ---- \n"
 
-echo "\n ---- 获取podspec文件 begin ---- \n"
+echo "\n ---- 正在获取podspec文件 ---- \n"
 
-# 获取到的文件路径
 file_path=""
 file_name=""
-# 文件后缀名
 file_extension="podspec"
-# 文件夹路径，pwd表示当前文件夹
 directory="$(pwd)"
 
-# 参数1: 路径；参数2: 文件后缀名
 function getFileAtDirectory(){
     for element in `ls $1`
-    do  
+    do
         dir_or_file=$1"/"$element
-        # echo "$dir_or_file"
         if [ -d $dir_or_file ]
         then 
             getFileAtDirectory $dir_or_file
         else
             file_extension=${dir_or_file##*.}
-            if [[ $file_extension == $2 ]]; then
-            	echo "$dir_or_file 是 $2 文件"
+            if [[ $file_extension == $2 ]];
+            then
+            	echo "\n ---- $dir_or_file 是 $2 文件 ---- "
             	file_path=$dir_or_file
             	file_name=$element
             fi
@@ -35,102 +31,51 @@ function getFileAtDirectory(){
 }
 getFileAtDirectory $directory $file_extension
 
-echo "\n -------------"
-echo "\n file_path: ${file_path}"
-echo "\n file_name: ${file_name}"
+echo "\n ---- file_path: ${file_path} ---- \n"
+echo "\n ---- file_name: ${file_name} ---- \n"
 
-echo "\n ---- 获取podspec文件 end ---- \n"
-
-
-
-echo "\n ---- 读取podspec文件内容 begin ---- \n"
-
-# 定义pod文件名称
+echo "\n ---- 开始读取podspec文件内容 ---- \n"
 pod_file_name=${file_name}
-# 查找 podspec 的版本
 search_str="s.version"
-
-# 读取podspec的版本
 podspec_version=""
+my_file="${pod_file_name}"
 
-#定义了要读取文件的路径
-my_file="${pod_file_name}" 
 while read my_line
 do
-#输出读到的每一行的结果
-# echo $my_line
-
-	# 查找到包含的内容，正则表达式获取以 ${search_str} 开头的内容
 	result=$(echo ${my_line} | grep "^${search_str}")
 	if [[ "$result" != "" ]]
 	then
    		echo "\n ${my_line} 包含 ${search_str}"
-
-   		# 分割字符串，是变量名称，不是变量的值; 前面的空格表示分割的字符，后面的空格不可省略
-		array=(${result// / })  
-		# 数组长度
+		array=(${result// / })
 		count=${#array[@]}
-		# 获取最后一个元素内容
 		version=${array[count - 1]}
-		# 去掉 '
 		version=${version//\'/}
-
 		podspec_version=$version
-	#else
-   		# echo "\n ${my_line} 不包含 ${search_str}"
 	fi
 
 done < $my_file
-
-echo "\n -------------"
-echo "\n podspec_version: ${podspec_version}"
-
-echo "\n ---- 读取podspec文件内容 end ---- \n"
-
-
+echo "\n ---- podspec_version: ${podspec_version} ---- \n"
 
 pod_spec_name=${file_name}
 pod_spec_version=${podspec_version}
 
-echo "\n****** ${pod_spec_name} ${pod_spec_version} begin ****** \n"
+echo "\n ---- ${pod_spec_name} ${pod_spec_version} ---- \n"
 
-echo "\n ------ 执行 git 本地提交代码操作 ------ \n"
-# git 操作
-echo "git add ."
+echo "\n ----- 执行 git 本地提交代码操作 ----- \n"
 git add .
-echo "git status"
 git status
-echo "git commit -m ${git_commit_des}"
 git commit -m ${git_commit_des}
-
-echo "\n ------ 执行 git 打标签tag，并推送到远端 ------ \n"
-# git推送到远端
-echo "git tag ${pod_spec_version}"
 git tag ${pod_spec_version}
-echo "git push origin master --tags"
 git push origin main --tags
 
-echo "\n ------ 执行 pod 本地校验 ------ \n"
-# pod 本地校验
-echo "pod spec lint --allow-warnings --verbose"
+echo "\n ---- pod正在校验 ---- \n"
 pod spec lint ${pod_spec_name} --allow-warnings --verbose
 
-echo "\n ------ 执行 pod 远端校验 ------ \n"
-# pod 远端校验
-echo "pod spec lint --allow-warnings --verbose"
-pod spec lint --allow-warnings --verbose
-
-echo "\n ------ 执行 pod 发布 ------ \n"
-# 发布
-echo "pod trunk push --allow-warnings"
+echo "\n ---- pod正在发布 ---- \n"
 pod repo push MindImCoreSpec ${pod_spec_name} --allow-warnings
-
-echo "\n****** ${pod_spec_name} ${pod_spec_version} end ****** \n"
-
-echo "\n****** pod repo update ****** \n"
 pod repo update
 
-echo "****** end ******"
+echo "\n ---- 升级SDK成功 ---- \n"
 
 
 
